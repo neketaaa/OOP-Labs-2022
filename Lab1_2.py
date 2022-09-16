3b3988e99b42889e83c65fddfdc04bd5dadbb7b1
neketaaa <nikita16060415@gmail.com>
2022-09-16 20:07:42 +0300

    ==false to not

--- Lab1_2.py
+++ Lab1_2.py
@@ -2,6 +2,7 @@
 import argparse
 import math
 import operator
+
 parser = argparse.ArgumentParser()
 parser.add_argument('action', type=str)
 parser.add_argument('first_number', type=float)
@@ -9,6 +10,6 @@ parser.add_argument('second_number', type=float)
 args = parser.parse_args()
 function = getattr(math, args.action, False)
 # False is default value
-if function == False:
+if not function:
     function = getattr(operator, args.action)
 print(function(args.first_number, args.second_number))
