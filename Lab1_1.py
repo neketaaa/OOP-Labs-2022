5a697bd3953d4f8262c4a0fe73e678980f5ff77c
neketaaa <nikita16060415@gmail.com>
2022-09-16 19:02:11 +0200

    Zero Division and Types

--- Lab1_1.py
+++ Lab1_1.py
@@ -1,20 +1,29 @@
 # Lab1_1
 import argparse
+
 parser = argparse.ArgumentParser()
 parser.add_argument('symbol', type=str)
-parser.add_argument('first_number', type=float)
-parser.add_argument('second_number', type=float)
+parser.add_argument('first_number')
+parser.add_argument('second_number')
 args = parser.parse_args()
-match args.symbol:
-    case '+':
-        print(args.first_number + args.second_number)
-    case '-':
-        print(args.first_number - args.second_number)
-    case '*':
-        print(args.first_number * args.second_number)
-    case '/':
-        print(args.first_number / args.second_number)
-    case _:
-        print('Invalid command')
-
+if args.first_number.isdigit() and args.second_number.isdigit():
+    try:
+        match args.symbol:
+            case '+':
+                print(int(args.first_number) + int(args.second_number))
+            case '-':
+                print(int(args.first_number) - int(args.second_number))
+            case '*':
+                print(int(args.first_number) * int(args.second_number))
+            case '/':
+                try:
+                    print(int(args.first_number) / int(args.second_number))
+                except ZeroDivisionError:
+                    print('Impossible zero division')
+            case _:
+                print('Invalid command')
+    except TypeError:
+        print('Wrong arguments type')
+else:
+    print('Wrong argument Type')
 
