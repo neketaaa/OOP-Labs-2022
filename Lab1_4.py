4a67afa5156bbbd67a6bb3b0b68c2bfd90f633b3
neketaaa <nikita16060415@gmail.com>
2022-09-18 03:04:59 +0300

    Update Lab1_4.py

--- Lab1_4.py
+++ Lab1_4.py
@@ -1,8 +1,23 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-W', type=int, default=0)
-parser.add_argument('-w', type=int, nargs="+", default=0)
-parser.add_argument('-n', type=int, default=0)
-args = parser.parse_args()
-print(args.W, args.w, args.n)
+def knapsak(W, w, n):
+    print(W, w, n)
+    table = [[0] * (W-1) for i in range(n)]
+    for i in range(1,W-1):
+        if w[0] < i or w[0] == i:
+            print(w[0],i, w[0]>i)
+            table[0][i] = w[0]
+    print(table)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-W', type=int, default=0)
+    parser.add_argument('-w', type=int, nargs="+", default=0)
+    parser.add_argument('-n', type=int, default=0)
+    args = parser.parse_args()
+    knapsak(args.W, args.w, args.n)
+
+if __name__ == "__main__":
+    main()
+
+
