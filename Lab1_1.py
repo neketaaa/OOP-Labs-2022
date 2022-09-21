2b5bf6a00d5d972fa1fedef0821b1eb007cfbcae
neketaaa <nikita16060415@gmail.com>
2022-09-22 00:48:26 +0300

    Improved algorithm

--- Lab1_1.py
+++ Lab1_1.py
@@ -6,24 +6,21 @@ parser.add_argument('symbol', type=str)
 parser.add_argument('first_number')
 parser.add_argument('second_number')
 args = parser.parse_args()
-if args.first_number.isdigit() and args.second_number.isdigit():
-    try:
-        match args.symbol:
-            case '+':
-                print(int(args.first_number) + int(args.second_number))
-            case '-':
-                print(int(args.first_number) - int(args.second_number))
-            case '*':
-                print(int(args.first_number) * int(args.second_number))
-            case '/':
-                try:
-                    print(int(args.first_number) / int(args.second_number))
-                except ZeroDivisionError:
-                    print('Impossible zero division')
-            case _:
-                print('Invalid command')
-    except TypeError:
-        print('Wrong arguments type')
-else:
-    print('Wrong argument Type')
+try:
+    match args.symbol,args.first_number.isdigit(), args.second_number.isdigit():
+        case '+',True,True:
+            print(int(args.first_number) + int(args.second_number))
+        case '-',True,True:
+            print(int(args.first_number) - int(args.second_number))
+        case '*',True,True:
+            print(int(args.first_number) * int(args.second_number))
+        case '/',True,True:
+            try:
+                print(int(args.first_number) / int(args.second_number))
+            except ZeroDivisionError:
+                print('Impossible zero division')
+        case _:
+            print('Invalid command or wrong type')
+except TypeError:
+    print('Wrong arguments type')
 
