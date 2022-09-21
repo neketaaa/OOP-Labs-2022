8d32916910b9c78599bc5d7ccea27dd900bf3ce7
neketaaa <nikita16060415@gmail.com>
2022-09-22 00:48:49 +0300

    takes 0+ args

--- Lab1_2.py
+++ Lab1_2.py
@@ -5,8 +5,8 @@ import operator
 
 parser = argparse.ArgumentParser()
 parser.add_argument('action', type=str)
-parser.add_argument('first_number', type=float)
-parser.add_argument('second_number', type=float)
+parser.add_argument('first_number', nargs='*', type=float)
+parser.add_argument('second_number', nargs='*', type=float)
 args = parser.parse_args()
 function = getattr(math, args.action, False)
 # False is default value
