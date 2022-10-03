7c358c6bbbeab93b9ef8dca8fb2c55795b414524
neketaaa <nikita16060415@gmail.com>
2022-10-03 03:00:24 +0300

    added exception

--- Lab1 P1/Lab1_2.py
+++ Lab1 P1/Lab1_2.py
@@ -8,8 +8,11 @@ parser.add_argument('action', type=str)
 parser.add_argument('first_number', nargs='*', type=float)
 parser.add_argument('second_number', nargs='*', type=float)
 args = parser.parse_args()
-function = getattr(math, args.action, False)
+try:
+    function = getattr(math, args.action, False)
 # False is default value
-if not function:
-    function = getattr(operator, args.action)
-print(function(args.first_number, args.second_number))
+    if not function:
+        function = getattr(operator, args.action)
+    print(function(args.first_number, args.second_number))
+except AttributeError:
+    print('No such command')
