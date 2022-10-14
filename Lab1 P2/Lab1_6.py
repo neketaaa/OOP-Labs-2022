7808637e7a7b84cd6fa41b64f58b250c31dde7d6
neketaaa <nikita16060415@gmail.com>
2022-10-15 00:27:24 +0300

    updated __init__

--- Lab1 P2/Lab1_6.py
+++ Lab1 P2/Lab1_6.py
@@ -1,19 +1,22 @@
 import argparse
 import math
 
+
 class Rational:
-    def __init__(self, num_value = 1, den_value = 1):
-        if isinstance(num_value, int):
-            if isinstance(den_value, int) and den_value:
-                gcd = math.gcd(num_value,den_value)
-                self.numerator = num_value / gcd
-                self.denominator = den_value / gcd
+    def __init__(self, num_value=1, den_value=1):
+        if isinstance(num_value, int) and isinstance(den_value, int) and den_value:
+            gcd = math.gcd(num_value, den_value)
+            self.numerator = num_value / gcd
+            self.denominator = den_value / gcd
+        else:
+            raise ValueError
 
     def show_int(self):
         return f'{self.numerator}/{self.denominator}'
 
     def show_flt(self):
-        return float(self.numerator)/float(self.denominator)
+        return float(self.numerator) / float(self.denominator)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -24,6 +27,7 @@ def main():
     example = Rational(args.numerator, args.denominator)
     print(example.show_int())
     print(example.show_flt())
+
 
 if __name__ == "__main__":
     main()
