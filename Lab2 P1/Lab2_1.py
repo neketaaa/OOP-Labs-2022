ab182c7b77a48b7bef9ce5a11e2c092c6c87461a
neketaaa <nikita16060415@gmail.com>
2022-10-30 19:34:50 +0300

    improved date

--- Lab2 P1/Lab2_1.py
+++ Lab2 P1/Lab2_1.py
@@ -1,13 +1,13 @@
 import random
 import string
 import json
-import datetime
+from datetime import datetime, timedelta
 
 
-def get_random_key():
-    letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
-    result_str = ''.join(random.choice(letters) for i in range(6))
-    return result_str
+# def get_random_key():
+#     letters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+#     result_str = ''.join(random.choice(letters) for i in range(6))
+#     return result_str
 
 
 class Ticket:
@@ -15,6 +15,7 @@ class Ticket:
     def __init__(self, number, price):
         self.number = number
         self.price = price
+        self.date = str(datetime.now())
 
 
     @property
@@ -72,12 +73,13 @@ class Late_ticket(Ticket):
 
 class Event:
 
-    def __init__(self, name, amount, price):
+    def __init__(self, name, amount, price, date: datetime):
         self.name = name
         self.amount = amount
         self.price = price
         self.list = {'tickets': []}
         self.act_number = 0
+        self.date = date
 
     @property
     def name(self):
@@ -112,6 +114,17 @@ class Event:
         else:
             raise ValueError
 
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, date):
+        if isinstance(date, datetime) and (date - datetime.now()).days > 0:
+            self.__date = date
+        else:
+            raise ValueError
+
     def push_json(self, item, file):
         item = json.dumps(item, indent=2)
         item = json.loads(str(item))
@@ -122,36 +135,41 @@ class Event:
         with open(file_name, 'r') as json_file:
             return json.load(json_file)['tickets'][index-1]
 
-    def sell_ticket(self, type):
-        key = get_random_key()
-        key = str(self.act_number +1)
-        # Type: R/A/S/L
+    def sell_ticket(self, isStudent: bool):
+        if not isinstance(isStudent, bool):
+            raise ValueError
         if self.act_number < self.amount:
-            match type:
-                case 'R':
-                    self.list['tickets'].append(Ticket(key, self.price).__dict__)
-                case 'A':
+            time = self.date - datetime.now()
+            key = str(self.act_number + 1)
+            if isStudent:
+                self.list['tickets'].append(Student_ticket(key, self.price).__dict__)
+            else:
+                if time.days > 60:
                     self.list['tickets'].append(Advanced_ticket(key, self.price).__dict__)
-                case 'S':
-                    self.list['tickets'].append(Student_ticket(key, self.price).__dict__)
-                case 'L':
-                    self.list['tickets'].append(Late_ticket(key, self.price).__dict__)
-            self.push_json(self.list, 'tickets.json')
+                else:
+                    if time.days < 10:
+                        self.list['tickets'].append(Late_ticket(key, self.price).__dict__)
+                    else:
+                        self.list['tickets'].append(Ticket(key, self.price).__dict__)
             self.act_number += 1
-            return self.pull_json('tickets.json', self.act_number - 1)
+            self.push_json(self.list, 'tickets.json')
+            return self.pull_json('tickets.json', self.act_number-1)
         else:
             return 'Sold Out'
 
 
-x = Event('Lesson', 5, 100)
-print(x.sell_ticket('S'))
-print(x.sell_ticket('R'))
-print(x.sell_ticket('S'))
-print(x.sell_ticket('A'))
-print(x.sell_ticket('S'))
-x.sell_ticket('L')
-x.sell_ticket('R')
-x.sell_ticket('S')
-x.sell_ticket('L')
-print(x.sell_ticket('S'))
-print(x.sell_ticket('A'))
+
+
+x = Event('Lesson', 10, 100, datetime(2022,11,15))
+print(x.sell_ticket(True))
+print(x.sell_ticket(False))
+print(x.sell_ticket(True))
+print(x.sell_ticket(False))
+print(x.sell_ticket(False))
+x.sell_ticket(False)
+x.sell_ticket(True)
+x.sell_ticket(False)
+x.sell_ticket(False)
+print(x.sell_ticket(True))
+print(x.sell_ticket(True))
+print(x.pull_json('tickets.json', 3))
