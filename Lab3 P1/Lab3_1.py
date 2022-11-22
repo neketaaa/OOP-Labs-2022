ef53cc6b960d665df7aa6285658426c1b994d9b1
neketaaa <nikita16060415@gmail.com>
2022-11-22 19:05:16 +0200

    Create Lab3_1.py

---
+++ Lab3 P1/Lab3_1.py
@@ -0,0 +1,125 @@
+import math
+
+
+class Rational:
+    def __init__(self, numerator=1, denominator=1):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    @property
+    def numerator(self):
+        return self.__numerator
+
+    @numerator.setter
+    def numerator(self, numerator):
+        if not isinstance(numerator, int):
+            raise ValueError
+        self.__numerator = numerator
+
+    @property
+    def denominator(self):
+        return self.__denominator
+
+    @denominator.setter
+    def denominator(self, denominator):
+        if not isinstance(denominator, int) and denominator != 0:
+            raise ValueError
+        self.__denominator = denominator
+
+    def __str__(self):
+        gcd = math.gcd(self.numerator, self.denominator)
+        return f'{self.numerator // gcd} / {self.denominator // gcd}'
+
+    def __add__(self, other):
+        if isinstance(other, Rational):
+            return Rational(self.numerator * other.denominator + other.numerator * self.denominator, self.denominator * other.denominator )
+        if isinstance(other, int):
+            return Rational(self.numerator + other*self.denominator, self.denominator)
+
+    def __radd__(self, other):
+        if isinstance(other, Rational):
+            return Rational(self.numerator * other.denominator + other.numerator * self.denominator, self.denominator * other.denominator )
+        if isinstance(other, int):
+            return Rational(self.numerator + other*self.denominator, self.denominator)
+
+    def __iadd__(self, other):
+        if isinstance(other, Rational):
+            return Rational(self.numerator * other.denominator + other.numerator * self.denominator, self.denominator * other.denominator )
+        if isinstance(other, int):
+            return Rational(self.numerator + other*self.denominator, self.denominator)
+
+    def __sub__(self, other):
+        if isinstance(other, Rational):
+            return Rational(self.numerator * other.denominator - other.numerator * self.denominator, self.denominator * other.denominator )
+        if isinstance(other, int):
+            return Rational(self.numerator - other*self.denominator, self.denominator)
+
+    def __mul__(self, other):
+        if isinstance(other, Rational):
+            return Rational(self.numerator * other.numerator, self.denominator * other.denominator)
+        if isinstance(other, int):
+            return Rational(self.numerator * other, self.denominator)
+
+    def __floordiv__(self, other):
+        if isinstance(other, Rational):
+            return Rational(self.numerator * other.denominator, self.denominator * other.numerator)
+        if isinstance(other, int):
+            return Rational(self.numerator, self.denominator * other)
+
+    def __truediv__(self, other):
+        if isinstance(other, Rational):
+            return Rational(int((self.numerator * other.denominator) / (self.denominator * other.numerator)), 1)
+        if isinstance(other, int):
+            return Rational(int(self.numerator / (self.denominator * other)), 1)
+
+    def __gt__(self, other):
+        if isinstance(other, Rational):
+            return (self.numerator * other.denominator) > (other.numerator * self.denominator)
+        if isinstance(other, int):
+            return (self.numerator // self.denominator) > other
+
+    def __lt__(self, other):
+        if isinstance(other, Rational):
+            return (self.numerator * other.denominator) < (other.numerator * self.denominator)
+        if isinstance(other, int):
+            return (self.numerator // self.denominator) < other
+
+    def __eq__(self, other):
+        if isinstance(other, Rational):
+            return (self.numerator * other.denominator) == (other.numerator * self.denominator)
+        if isinstance(other, int):
+            return (self.numerator // self.denominator) == other
+
+    def __ge__(self, other):
+        if isinstance(other, Rational):
+            return (self.numerator * other.denominator) >= (other.numerator * self.denominator)
+        if isinstance(other, int):
+            return (self.numerator // self.denominator) >= other
+
+    def __le__(self, other):
+        if isinstance(other, Rational):
+            return (self.numerator * other.denominator) <= (other.numerator * self.denominator)
+        if isinstance(other, int):
+            return (self.numerator // self.denominator) <= other
+
+x = Rational(4,2)
+print(x)
+y = Rational(3,5)
+print(y)
+print(x+y)
+print(x+2)
+print(x-y)
+print(x-2)
+print(x*y)
+print(x*2)
+print(x//y)
+print(y//2)
+print(x/y)
+print(y/2)
+print(x>y)
+print(y>2)
+x += y
+print(x)
+
+
+
